d6ba5abbac2e96a100a0262c6e49b7d78fd44e6c
Damla-Altunay <damla.alltunay@ibm.com>
2021-01-15 23:54:56 +0300

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div class="app__header">
+        <h1>Covid-19 Dashboard</h1>
+        
+        <FormControl className="app-dropdown">
+          <Select 
+            variant="outlined"
+            value="abc"
+          >
+            <MenuItem value="A">A</MenuItem>
+            <MenuItem value="B">B</MenuItem>
+            <MenuItem value="C">C</MenuItem>
+            <MenuItem value="D">D</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title + Select input dropdown fielld */}
+
+      {/* info boxes */}
+      {/* info boxes */}
+      {/* info boxes */} 
+      {/* info boxes */} 
+
+      {/* info boxes */}
+      {/* info boxes */}
+      {/* info boxes */} 
+      {/* info boxes */} 
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+
     </div>
   );
 }
